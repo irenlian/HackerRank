a5518b1fbd1ad6f432afb804ce4f4c8ef318fdef
Somaliika <iren33333@gmail.com>
2020-02-04 22:24:35 +0200

    Refactor newYearChaos

--- exercises/newYearChaos.js
+++ exercises/newYearChaos.js
@@ -1,20 +1,19 @@
 // https://www.hackerrank.com/challenges/new-year-chaos/problem
 // For hacherrank should return in stdout
-function minimumBribes(q) {
-	let bribes = 0;
-	let sortedToIndex = -1;
-	do {
-    for (let i = sortedToIndex + 1; i < q.length - 1; i++) {
-      if (q[i] - (i + 1) > 2)
-        return "Too chaotic";
+function minimumBribes(queue) {
+  let bribes = 0;
+  let sortedToIndex = -1;
+  const q = [...queue];
+  do {
+    for (let i = sortedToIndex + 1; i < q.length - 1; i += 1) {
+      if (q[i] - (i + 1) > 2) return 'Too chaotic';
       if (q[i] > q[i + 1]) {
         [q[i], q[i + 1]] = [q[i + 1], q[i]];
-        bribes++;
+        bribes += 1;
       }
-      if (sortedToIndex === i - 1 && q[i] === i + 1)
-        sortedToIndex = i;
+      if (sortedToIndex === i - 1 && q[i] === i + 1) sortedToIndex = i;
     }
   } while (sortedToIndex < q.length - 2);
-    return bribes;
-};
+  return bribes;
+}
 module.exports = minimumBribes;
