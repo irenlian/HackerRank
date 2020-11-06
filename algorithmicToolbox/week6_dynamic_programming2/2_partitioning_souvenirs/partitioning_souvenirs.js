8a3c21aa21f01490da2d899f2e4185bc087a5f38
irenlian <irynalian@stockx.com>
2020-11-06 10:53:01 +0200

    Partitioning with dynamic programming

--- algorithmicToolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partitioning_souvenirs.js
+++ algorithmicToolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partitioning_souvenirs.js
@@ -14,82 +14,49 @@ rl.once('line', (line) => {
   });
 });
 
-function getMatrixOfElements(capacity, elems) {
-  const results = [new Array(capacity + 1).fill(0)];
+// Trying to optimize the recursive calls
+let memoization = [];
 
-  for (let i = 1; i <= elems.length; i++) {
-    const elemIndex = i - 1;
-    results[i] = [0];
-    for (let j = 1; j <= capacity; j++) {
-      results[i][j] = results[i - 1][j];
-      let previous = results[i - 1][j - elems[elemIndex]];
-      if (previous !== undefined
-        && previous + elems[elemIndex] <= capacity
-        && previous + elems[elemIndex] > results[i - 1][j]) {
-        results[i][j] = previous + elems[elemIndex];
-      }
-    }
+function checkValue(values, s1, s2, s3) {
+  let res = 0;
+  if (memoization[s1][s2][s3] > 0) {
+    res = memoization[s1][s2][s3];
+  } else {
+    res = partitioningRecursive(values.slice(1), s1, s2, s3);
+    memoization[s1][s2][s3] = res;
   }
-
-  return results;
+  return res;
 }
 
-function getSouvenirs(capacity, souvenirs) {
-  const matrix = getMatrixOfElements(capacity, souvenirs);
-
-  const chosen = [];
-  let i = souvenirs.length, j = capacity;
-  while (i > 0 && j > 0) {
-    const souvenir = souvenirs[i - 1];
-    if (matrix[i - 1][j] <= matrix[i - 1][j - souvenir] + souvenir) {
-      j -= souvenir;
-      chosen.push(souvenir);
-    }
-    i--;
-  }
-  return chosen;
-}
-
-function getArrayDifference(array1, array2) {
-  const initial = [...array1];
-  const excludes = [...array2];
-
-  for (let i = 0; i < initial.length; i++) {
-    for (let j = 0; j < excludes.length; j++) {
-      if (initial[i] === excludes[j]) {
-        initial.splice(i, 1);
-        excludes.splice(j, 1);
-        i--;
-        j--;
-      }
-    }
-  }
-  return initial;
-}
-
-function partitioningSouvenirs(souvenirs) {
-  // check whether souvenirs can be divided at whole
-  const sum = souvenirs.reduce((s, value) => s + value, 0);
-  if (sum % 3 !== 0) return 0;
-
-  // sort souvenirs to get bigger elements first
-  const sortedSouvenirs = souvenirs.sort((a, b) => a - b);
-
-  // get souvenirs for the first person
-  const firstPerson = getSouvenirs(sum / 3, sortedSouvenirs);
-  const souvenirsLeft = getArrayDifference(sortedSouvenirs, firstPerson);
-
-  // get souvenirs for the second person
-  const secondPerson = getSouvenirs(sum / 3, souvenirsLeft);
-
-  // get souvenirs for the third person
-  const thirdPerson = getArrayDifference(souvenirsLeft, secondPerson);
-  const sumOfThirdPerson = thirdPerson.reduce((s, value) => s + value, 0);
-
-  if (sumOfThirdPerson === sum / 3) {
+function partitioningRecursive(values, s1, s2, s3) {
+  if (s1 === 0 && s2 === 0 && s3 === 0) {
     return 1;
   }
-  return 0;
+  if (!values.length) {
+    return 0;
+  }
+  const results = [0];
+  if (s1 - values[0] >= 0) {
+    results.push(checkValue(values, s1 - values[0], s2, s3));
+  }
+  if (s2 - values[0] >= 0) {
+    results.push(checkValue(values, s1, s2 - values[0], s3));
+  }
+  if (s3 - values[0] >= 0) {
+    results.push(checkValue(values, s1, s2, s3 - values[0]));
+  }
+  return Math.max(...results);
+}
+
+function partitioningSouvenirs(values) {
+  // check whether souvenirs can be divided at whole
+  const sum = values.reduce((s, value) => s + value, 0);
+  if (sum % 3 !== 0) return 0;
+
+  memoization = Array(sum / 3 + 1).fill(-1);
+  memoization = memoization.map(() => Array(sum / 3 + 1).fill(-1));
+  memoization = memoization.map(() => [...memoization]);
+  return partitioningRecursive(values, sum / 3, sum / 3, sum / 3);
 }
 
 module.exports = partitioningSouvenirs;
--- algorithmicToolbox/week6_dynamic_programming2/2_partitioning_souvenirs/stressTest.js
+++ algorithmicToolbox/week6_dynamic_programming2/2_partitioning_souvenirs/stressTest.js
@@ -34,15 +34,17 @@ function stressTest() {
   // console.log(partitioningSouvenirs([17, 59, 34, 57, 17, 23, 67, 1, 18, 2, 59]) === 1);
   // console.log(partitioningSouvenirs([1, 2, 3, 4, 5, 5, 7, 7, 8, 10, 12, 19, 25]) === 1);
   // console.log(partitioningSouvenirs([ 1, 6, 2, 7, 8, 3 ]) === 1);
+  // console.log(partitioningSouvenirs([ 6, 7, 8, 9, 12 ]) === 0);
+  console.log(partitioningSouvenirs([
+    11, 1, 1, 10, 13,
+    8, 7, 7, 11
+  ]) === 1);
   while (true) {
     const souvenirsNumber = Math.floor(Math.random() * 10) + 1;
-    let souvenirs = [
-      1, 3,  5,  5,  6,
-      9, 9, 10, 10, 11
-    ];
-    // for (let i = 0; i < souvenirsNumber; i++) {
-    //   souvenirs.push(Math.floor(Math.random() * 13) + 1);
-    // }
+    let souvenirs = [];
+    for (let i = 0; i < souvenirsNumber; i++) {
+      souvenirs.push(Math.floor(Math.random() * 13) + 1);
+    }
     const capacity = souvenirs.reduce((s, value) => s + value, 0);
     const res1 = partitioningSouvenirsNaive(capacity / 3, souvenirs);
     const res2 = partitioningSouvenirs(souvenirs);
